35dfdfc4ebb5b68913da06abee9310cfb6a9ba1f
Daniel Sedlak <daniel.sedlak@365.pslib.cz>
2017-11-13 22:32:01 +0100

    add comments, and finish code

--- src/index.js
+++ src/index.js
@@ -7,19 +7,18 @@ const { app, BrowserWindow, ipcMain } = electron;
 let window;
 let homeDir;
 
-app.on("ready", startApp);
+app.on("ready", startApp); // Při startu instancuje okno
+
+// Event handler, který nasloouchá na události u okna (první parametr "view-data" je libovolný, jen musí být stejný jako ve view)
 ipcMain.on("view-data", (event, data) => {
-    console.log(data);
+    fs.writeFileSync(`${path.resolve(homeDir,"data.txt")}`, data, 'utf-8'); // Vytvoří a zapíše do souboru v domácim adresáři
 });
 
 function startApp() {
+    // Získá adresu složky, kam se pak následně vytvoří soubor s daty
+    // Jsou použity knihovny Node.js, aby se zaručila multiplatformnost (rozdíl lomítek na jiných OS)
     fs.mkdir(path.join(os.homedir(), "demo-app"), () => { homeDir = path.join(os.homedir(), "demo-app") });
 
-    if (window == null) {
-        window = new BrowserWindow({});
-        window.loadURL(`file://${path.resolve(__dirname, "..", "view", "index.html")}`);
-    }
-    else {
-        console.log("Window already created");
-    }
+    window = new BrowserWindow({}); // Instancovaní jednoho okna (vlákna)
+    window.loadURL(`file://${path.resolve(__dirname, "..", "view", "index.html")}`); // Načtení HTML do okna
 }

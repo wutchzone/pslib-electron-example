3206465fac060cfc4523523d20c5f18baf11bfeb
Daniel Sedlak <daniel.sedlak@365.pslib.cz>
2017-11-14 20:54:59 +0100

    Update README and comments

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# pslib-electron-example
+# Electron basic example
+This example shows basic features of electron.
+It opens window with textarea html tag and after typing it stores data to home directory (~/*userName*/demo-app/data.txt).
+
+# How to run
+1. Download it `git clone git@github.com:wutchzone/pslib-electron-example.git`.
+2. Install Node and NPM, check by typing `node -v && npm -v`.
+3. Navigate to the project directory `cd pslib-electron-example`.
+4. Install all dependencies `npm install`.
+5. Run project `npm start`.
--- src/index.js
+++ src/index.js
@@ -3,22 +3,26 @@ const os = require("os");
 const fs = require("fs");
 const path = require("path");
 
-const { app, BrowserWindow, ipcMain } = electron;
-let window;
+const { app, BrowserWindow, ipcMain } = electron; // ES6 nová syntaxe
+let window; //ES6 nový syntaxe, pouze informuje interpret že tato proměnná se bude používat
 let homeDir;
 
 app.on("ready", startApp); // Při startu instancuje okno
 
 // Event handler, který nasloouchá na události u okna (první parametr "view-data" je libovolný, jen musí být stejný jako ve view)
 ipcMain.on("view-data", (event, data) => {
-    fs.writeFileSync(`${path.resolve(homeDir,"data.txt")}`, data, 'utf-8'); // Vytvoří a zapíše do souboru v domácim adresáři
+  fs.writeFileSync(`${path.resolve(homeDir, "data.txt")}`, data, "utf-8"); // Vytvoří a zapíše do souboru v domácim adresáři
 });
 
 function startApp() {
-    // Získá adresu složky, kam se pak následně vytvoří soubor s daty
-    // Jsou použity knihovny Node.js, aby se zaručila multiplatformnost (rozdíl lomítek na jiných OS)
-    fs.mkdir(path.join(os.homedir(), "demo-app"), () => { homeDir = path.join(os.homedir(), "demo-app") });
+  // Získá adresu složky, kam se pak následně vytvoří soubor s daty
+  // Jsou použity knihovny Node.js, aby se zaručila multiplatformnost (rozdíl lomítek na jiných OS)
+  fs.mkdir(path.join(os.homedir(), "demo-app"), () => {
+    homeDir = path.join(os.homedir(), "demo-app");
+  });
 
-    window = new BrowserWindow({}); // Instancovaní jednoho okna (vlákna)
-    window.loadURL(`file://${path.resolve(__dirname, "..", "view", "index.html")}`); // Načtení HTML do okna
+  window = new BrowserWindow({}); // Instancovaní jednoho okna (vlákna)
+  window.loadURL(
+    `file://${path.resolve(__dirname, "..", "view", "index.html")}`
+  ); // Načtení HTML do okna ze složky view
 }
